71c7c6c1d6f837f94e539aae8f95161b6323d690
Dave <david.leslie@temple.edu>
2014-11-16 10:27:12 -0500

    added check for zenity, changed sudo to gksudo for first sudo in chain for dat gui

--- ps3connect.sh
+++ ps3connect.sh
@@ -1,16 +1,27 @@
- #!/bin/bash
- sudo apt-add-repository ppa:falk-t-j/qtsixa #> /dev/null
- echo "repo"
- #might want to get rid of update
- sudo apt-get update #> /dev/null
- echo "update"
- sudo apt-get install sixad #> /dev/null
- zenity --info --text="Connect the PS3 controller"
- sudo sixpair #> /dev/null
- zenity --info --text="Unplug the PS3 controller"
- zenity --info --text="Press the home button!" 
+#!/bin/bash
+#check for zenity and attempt to install if not found
+PKG_FOUND = $(dpkg-query -W --showformat='$(Status)\n'zenity|grep "install ok installed")
+echo Checking for lub: $PKG_FOUND
+if ["" == PKG_FOUND]; then
+  echo "couldn't find"
+  gksudo apt-get --force-yes --yes install zenity
+fi
+
+#zenity *could* be used to pipe the password to sudo -A (i think),
+#but storing the pword in plain text in mem might be retarded
+#gksudo is a better option for gui pword	      
+gksudo apt-add-repository ppa:falk-t-j/qtsixa #> /dev/null
+echo "repo"
+#might want to get rid of update
+sudo apt-get update > /dev/null
+echo "update"
+sudo apt-get install sixad #> /dev/null
+zenity --info --text="Connect the PS3 controller"
+sudo sixpair #> /dev/null
+zenity --info --text="Unplug the PS3 controller"
+zenity --info --text="Press the home button!" 
  
- sudo sixad --start #> /dev/null
+sudo sixad --start #> /dev/null
 
 
  
