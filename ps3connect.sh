9f6c723ac596c8d32d92b8edd6f6adf55abc88df
Dave <david.leslie@temple.edu>
2014-11-16 12:20:57 -0500

    updated shell

--- ps3connect.sh
+++ ps3connect.sh
@@ -3,19 +3,25 @@
 #this will try to find the package and store the result of a grep on the return
 #using keywords. if it can't be found the string will be empty and the module 
 #attempted to be installed
-if [$(dpkg-query -W -f='${Status}' nano 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
-  gksudo apt-get --force-yes --yes install zenity
+checked = 0;
+if [ $(dpkg-query -W -f='${Status}' zenity 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
+ gksudo apt-get install zenity > /dev/null
+ checked = 1;
 fi
 
 #zenity *could* be used to pipe the password to sudo -A (i think),
 #but storing the pword in plain text in mem might be retarded
 #gksudo is a better option for gui pword and is ALMOST always
 #installed by default	      
-gksudo apt-add-repository ppa:falk-t-j/qtsixa > /dev/null #no one wants to see all that
+if [ $checked -eq 0]; then
+  gksudo apt-add-repository ppa:falk-t-j/qtsixa > /dev/null #no one wants to see all that
+else
+  sudo apt-add-repository ppa:falk-t-j/qtsixa > /dev/null
+fi
 #update repos
 sudo apt-get update > /dev/null
 echo "update"
-sudo apt-get install sixad #> /dev/null
+sudo apt-get install sixad > /dev/null
 zenity --info --text="Connect the PS3 controller"
 sudo sixpair #> /dev/null
 zenity --info --text="Unplug the PS3 controller"
--- remove_ps3.sh
+++ remove_ps3.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sudo apt-get --force-yes --yes remove zenity > /dev/null
 sudo apt-get --force-yes --yes remove sixad > /dev/null
-sudo apt-get --force-yes --yes remove sixpair > /dev/null
 sudo apt-get --force-yes --yes autoremove > /dev/null
 echo "Removed some shit"
