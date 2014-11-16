ac49a45e0b2415dc6bd7b7c6c133118c93c21daa
Dave <david.leslie@temple.edu>
2014-11-16 11:14:00 -0500

    shell script probably. remove stuff script definitely.

--- ps3connect.sh
+++ ps3connect.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #check for zenity and attempt to install if not found
-PKG_FOUND = $(dpkg-query -W --showformat='$(Status)\n'zenity|grep "install ok installed")
-echo Checking for lub: $PKG_FOUND
-if ["" == PKG_FOUND]; then
-  echo "couldn't find"
+#this will try to find the package and store the result of a grep on the return
+#using keywords. if it can't be found the string will be empty and the module 
+#attempted to be installed
+if [$(dpkg-query -W -f='${Status}' nano 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
   gksudo apt-get --force-yes --yes install zenity
 fi
 
 #zenity *could* be used to pipe the password to sudo -A (i think),
 #but storing the pword in plain text in mem might be retarded
-#gksudo is a better option for gui pword	      
-gksudo apt-add-repository ppa:falk-t-j/qtsixa #> /dev/null
-echo "repo"
-#might want to get rid of update
+#gksudo is a better option for gui pword and is ALMOST always
+#installed by default	      
+gksudo apt-add-repository ppa:falk-t-j/qtsixa > /dev/null #no one wants to see all that
+#update repos
 sudo apt-get update > /dev/null
 echo "update"
 sudo apt-get install sixad #> /dev/null
