a098453b964d985d824b2912b1fde8014546f100
Dave <david.leslie@temple.edu>
2014-12-08 16:38:43 -0500

    added difflog, changed text outputs that had bad words in them

--- remove_ps3.sh
+++ remove_ps3.sh
@@ -2,4 +2,3 @@
 sudo apt-get --force-yes --yes remove zenity > /dev/null
 sudo apt-get --force-yes --yes remove sixad > /dev/null
 sudo apt-get --force-yes --yes autoremove > /dev/null
-echo "Removed some shit"
